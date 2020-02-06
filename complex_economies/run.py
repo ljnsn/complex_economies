d6a849ef75b3279e827cc8e2ba5ee3c044c696d6
iuvbio <lyndonbjohn@gmail.com>
2020-02-06 02:06:11 +0100

    move log to analysis and log exception

--- complex_economies/run.py
+++ complex_economies/run.py
@@ -1,20 +1,12 @@
 
-from datetime import datetime
 import logging
 
 from complex_economies.model import ComplexEconomy
 
 
-log = logging.getLogger()
-log.setLevel('DEBUG')
-fh = logging.FileHandler(f'model_{int(datetime.now().timestamp())}.log')
-fh.setLevel('DEBUG')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-log.addHandler(fh)
-
-
 def run(steps, params, init_conditions, seed=None):
+
+    log = logging.getLogger(__name__)
 
     m = ComplexEconomy(
         parameters=params,
@@ -31,6 +23,7 @@ def run(steps, params, init_conditions, seed=None):
         try:
             m.step()
         except Exception:
+            log.exception(f'The run stopped early at step {n}')
             print(f'The run stopped early at step {n}')
             return m
     return m
